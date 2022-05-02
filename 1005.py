1c40caa017ff39ad24834b943b2b825322482c0e
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-05-02 18:53:09 +0900

    Update 1005.py

--- 1005.py
+++ 1005.py
@@ -1,7 +1,21 @@
 t = int(input())
+time = 0
 
 for i in range(t):
     n, k = map(int, input().split())
     d = map(int, input().split())
-    for i in range(k+2):
-        
+    d = list(d)
+    d1 = []
+    for d2 in d:
+        d3 = []
+        d3.append(d2)
+        d1.append(d3)
+    d = d1
+    time += d[0][0]
+    for ii in range(k):
+        x, y = map(int, input().split())
+        d[x-1].append(y)
+    w = int(input())
+    for ii in range():
+        time += max(d[ii])
+    print(time)
