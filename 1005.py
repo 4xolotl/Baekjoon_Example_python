f966f715b13c8cc00150f90b4df63c6752ca4578
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-05-03 02:37:54 +0900

    Update 1005.py

--- 1005.py
+++ 1005.py
@@ -1,21 +1,41 @@
 t = int(input())
 time = 0
 
+def tectree(r, x, y):
+    tmp=0
+    if x in r:
+        for i in range(len(r)):
+            for ii in range(len(r[i])):
+                if x == ii:
+                    r[i+1].append(y)
+                    tmp = 1
+                    break
+            if tmp == 1:
+                break
+    elif y in r[:]:
+        for i in range(len(r)):
+            for ii in range(len(r[i])):
+                if y == ii:
+                    r[i].append(x)
+                    tmp = 1
+                    break
+            if tmp == 1:
+                break
+    else:
+        r[x-1].append(x)
+        r[x].append(y)
+    return r
+    
+
 for i in range(t):
+    r = []
     n, k = map(int, input().split())
-    d = map(int, input().split())
-    d = list(d)
-    d1 = []
-    for d2 in d:
-        d3 = []
-        d3.append(d2)
-        d1.append(d3)
-    d = d1
-    time += d[0][0]
+    for ii in range(n):
+        r.append([])
+    D = list(map(int, input().split()))
+    tech = dict(zip(list(range(1, n+1)), D))
+    print(tech)
     for ii in range(k):
-        x, y = map(int, input().split())
-        d[x-1].append(y)
-    w = int(input())
-    for ii in range():
-        time += max(d[ii])
-    print(time)
+        a = list(map(int, input().split()))
+        r = tectree(r, a[0], a[1])
+        print(r)
