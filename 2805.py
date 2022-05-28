43778de7f317acdeff6a69d3932009b98f685404
catgolgol <59068240+catgolgol@users.noreply.github.com>
2022-05-29 04:14:06 +0900

    Update 2805.py

--- 2805.py
+++ 2805.py
@@ -1,10 +1,16 @@
 n, m = map(int, input().split())
 ar1 = list(map(int, input().split()))
 
-for i in range(max(ar1), 0, -1):
-    s1=0
-    for ii in range(n):
-        s1 += ar1[ii]-i if (ar1[ii] > i) else 0
-    if s1 >= m:
-        print(i)
-        break
+def target(ar, n):
+    st, en = 0, n
+    while st <= en:
+        mid = (st + en) // 2
+        if ar[mid] == t:
+            return 1
+        elif ar[mid] > t:
+            en = mid - 1
+        else:
+            st = mid + 1
+    return 0
+
+target(ar1, m)
